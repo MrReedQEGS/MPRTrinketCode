4e10ea99977216ebf87a6b9dfe7d9a3f2ed241dd
MrReedQEGS <60434301+MrReedQEGS@users.noreply.github.com>
2026-01-21 10:26:17 +0000

    Create step1 - for loop to print a list.py

---
+++ KS3/lessons/lists/step1 - for loop to print a list.py
@@ -0,0 +1,14 @@
+#################################################
+# HOW TO PRINT A LIST USING A FOR LOOP
+#
+# Written by Mr Reed
+#################################################
+
+myListOfThings = ["cat","dog","horse","spider","ant","bear"]
+
+#print a title
+print("My list of things:")
+
+#Use a for loop to print out each thing one at a time
+for something in myListOfThings:
+  print(something)
