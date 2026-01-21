43692c398ad02285e8d9b1e63b5f23f2998903f5
MrReedQEGS <60434301+MrReedQEGS@users.noreply.github.com>
2026-01-21 10:27:18 +0000

    Create step1b - printing just one thing.py

---
+++ KS3/lessons/lists/step1b - printing just one thing.py
@@ -0,0 +1,20 @@
+#################################################
+# HOW TO PRINT ONE THING FROM A LIST
+#
+# Written by Mr Reed
+#################################################
+
+myListOfThings = ["cat","dog","horse","spider","ant","bear"]
+# POSITIONS         0    1      2        3       4     5
+
+#If I want to print just one thing from a list, then 
+# you need to know what POSITION it is in.
+
+# print the thing in position 2 -> horse
+print(myListOfThings[2])  
+
+# print the thing in position FIRST thing -> cat
+print(myListOfThings[0]) 
+
+# print the thing in position 5 -> bear
+print(myListOfThings[5]) 
