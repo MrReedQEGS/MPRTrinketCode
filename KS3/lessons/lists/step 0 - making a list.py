cfaf40cb0775cf6b7f6e539a7fb992731289e934
MrReedQEGS <60434301+MrReedQEGS@users.noreply.github.com>
2026-01-21 10:25:40 +0000

    Create step 0 - making a list.py

---
+++ KS3/lessons/lists/step 0 - making a list.py
@@ -0,0 +1,14 @@
+#################################################
+# HOW TO MAKE A LIST OF THINGS IN PYTHON
+#
+# Written by Mr Reed
+#################################################
+
+#Use square brackets and separate things with commas.
+
+myListOfThings = ["cat","dog","horse","spider","ant"]
+
+#It can then be printed out in a very basic way like this:
+print(myListOfThings)
+
+
