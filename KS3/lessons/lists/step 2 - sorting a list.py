e43fdab546285a26562e86a3bf156e2bbe9de74e
MrReedQEGS <60434301+MrReedQEGS@users.noreply.github.com>
2026-01-21 10:28:28 +0000

    Create step 2 - sorting a list.py

---
+++ KS3/lessons/lists/step 2 - sorting a list.py
@@ -0,0 +1,19 @@
+#################################################
+# HOW TO SORT A LIST
+#
+# Written by Mr Reed
+#################################################
+
+animals = ["cow","ant","frog","ant"]
+
+#print them
+print("Before sorting...")
+print(animals)
+print()
+
+#sort them into order!
+animals.sort()
+
+#print them again
+print("Now in order...")
+print(animals)
